a8d98b3a108fc00f1e11ea7df54ce2728b1727a8
AMS-JR <asj.sarjo@gmail.com>
2024-12-04 16:45:25 -0600

    Commit fundamentals

---
+++ fundamentals/src/main/java/array_duplicate_strings/Prog4.java
@@ -0,0 +1,26 @@
+package array_duplicate_strings;
+
+import java.util.Arrays;
+
+public class Prog4 {
+    public static void main(String[] args){
+        String [] inputArray = {"horse", "dog", "horse", "dog", "cat", "cow","goat","goat"};
+        String [] result = new String[0];
+
+        for (String string : inputArray) {
+            boolean notDuplicate = true;
+            for (String s : result) {
+                if (string.equals(s)) {
+                    notDuplicate = false;
+                    break;
+                }
+            }
+            if (notDuplicate) {
+                result = Arrays.copyOf(result, (result.length + 1));
+                result[result.length - 1] = string;
+            }
+        }
+        System.out.println(Arrays.toString(result));
+    }
+
+}
---
+++ fundamentals/src/main/java/second_min/Prog6.java
@@ -0,0 +1,27 @@
+package second_min;
+
+public class Prog6 {
+
+    public static void main(String[] args){
+
+        secondMin(new int[]{4, 2, 36, 0, 44, 32, 1});
+        // test case
+//        secondMin(new int[]{4, 2, 36, 1, 44, 32, 0});
+
+    }
+    public static void secondMin(int[] arrayOfInts){
+        int min = arrayOfInts[0];
+        int secondMin = arrayOfInts[1];
+
+        for (int n: arrayOfInts){
+            if(n<secondMin && n>min){
+                secondMin = n;
+            }
+            else if(n<min){
+                secondMin = min;  // update second min if current number is smaller than min
+                min = n;
+            }
+        }
+        System.out.println(secondMin);
+    }
+}
