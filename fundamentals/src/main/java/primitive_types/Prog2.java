8428e169e384e2283fe010bd4fe919eec2361358
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2024-12-05 04:49:19 -0600

    Commit updated primitives type

--- fundamentals/src/main/java/primitive_types/Prog2.java
+++ fundamentals/src/main/java/primitive_types/Prog2.java
@@ -1,9 +1,11 @@
-// package Problem2;
+package primitive_types;
 
 public class Prog2 {
 
     public static void main(String [] args){
-        float f1 = 1.27f, f2 = 3.881f, f3 = 9.6f;
+        float f1 = 1.27f;
+        float f2 = 3.881f;
+        float f3 = 9.6f;
         int sum = (int)(f1+f2+f3);
         System.out.println(sum);
 
