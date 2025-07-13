0aeaf100c7b15b12c35b0a1e54190ad688fdfa09
AMS-JR <asj.sarjo@gmail.com>
2025-07-13 17:13:12 -0500

    Commit String Constructor

---
+++ strings/src/main/java/constructors/CharArray.java
@@ -0,0 +1,9 @@
+package constructors;
+
+public class CharArray {
+    public static void main(String[] args) {
+        char[] charArray = {'h','e','l','l','o','.'};
+        String helloString = new String(charArray);
+        System.out.println(helloString);
+    }
+}
